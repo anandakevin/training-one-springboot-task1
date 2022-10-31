f7b7d38ac88816b03a5361f020ccc1dc3c8448ed
Ananda Kevin Refaldo Sariputra <anandakevinsariputra@gmail.com>
2022-11-01 00:50:09 +0700

    Added little configuration of spring security

--- src/main/java/com/training/one/task1/config/SecurityConfiguration.java
+++ src/main/java/com/training/one/task1/config/SecurityConfiguration.java
@@ -2,8 +2,28 @@ package com.training.one.task1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests()
+                .mvcMatchers(HttpMethod.GET, "/**").permitAll()
+                .mvcMatchers(HttpMethod.POST, "/**").permitAll()
+                .mvcMatchers(HttpMethod.PUT, "/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+        return http.build();
+    }
 }
